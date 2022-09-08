22c9356e616020d7bd859606de8d55d8abad5b6a
CrownVicPI437 <81381958+CrownVicPI437@users.noreply.github.com>
2022-09-08 01:59:09 -0500

    adding all files into git

---
+++ ViewModelClass/ComponentVMs/CardComponentVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ViewModelClass;
+
+public class CardComponentVM
+{
+    public string? HeaderOneInput { get; set; }
+    public string? BodyOneInput { get; set; }
+    //I started out taking anything apart to figure out how it works. In the begging I didn’t always get things back to together correctly, but thankfully I got better with practice. This was the start to my pasination for anything with wheels. Quickly I figured out my passion was in cars. In high-school I went through the Auto-tech program. I then got a Job at My local Ford dealer out of high-school. After a while I decided to go to college where I graduated from the Ford ASSET program with my Associates in Applied Science. After being at the dealer for 16 years I found I wanted to help customers more than I could at the dealer. Now I’m focusing on helping people gain confidence to make good decisions on maintenance and repairs for their vehicle.
+    public string? HeaderTwoInput { get; set; }
+    public string? BodyTwoInput { get; set; }
+    public string? HeaderThreeInput { get; set; }
+    public string? BodyThreeInput { get; set; }
+}
