8ce86609f1a3e5fd907a51151784e5e07c86948d
CrownVicPI437 <81381958+CrownVicPI437@users.noreply.github.com>
2023-01-05 13:27:43 -0600

    Adding in the email function for this update and starting on user login and information database

--- BlazorApp1/Shared/HomePageLayout.razor.cs
+++ BlazorApp1/Shared/HomePageLayout.razor.cs
@@ -8,11 +8,14 @@ namespace BlazorApp1.Shared;
 
 public partial class HomePageLayout
 {
+    [CascadingParameter] public IModalService BlazorModal { get; set; }
+    
     private CardComponentVM ComponentVm { get; set; } = new CardComponentVM();
     private bool Width { get; set; } = false;
 
     public void SendEmail()
     {
-        
+        var options = new ModalOptions { Size = ModalSize.Medium};
+        BlazorModal.Show<EmailModal>("Get in touch with us" , options );
     }
 }
